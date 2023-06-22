d0e9fb907d78e2a0157c31607d597a46748c781e
Niels <n.koster@portavita.eu>
2023-06-22 23:28:39 +0200

    Add route that can fetch user info from GitHub

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,9 +5,10 @@
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/greet/[name].tsx";
-import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/random-uuid.tsx";
+import * as $3 from "./routes/github/[username].tsx";
+import * as $4 from "./routes/greet/[name].tsx";
+import * as $5 from "./routes/index.tsx";
+import * as $6 from "./routes/random-uuid.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -15,9 +16,10 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/about.tsx": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/greet/[name].tsx": $3,
-    "./routes/index.tsx": $4,
-    "./routes/random-uuid.tsx": $5,
+    "./routes/github/[username].tsx": $3,
+    "./routes/greet/[name].tsx": $4,
+    "./routes/index.tsx": $5,
+    "./routes/random-uuid.tsx": $6,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
