5695bcad54b5ae1fb3a125a7f4d725d8dc95b253
Niels <n.koster@portavita.eu>
2023-06-22 21:18:54 +0200

    Use destructuring for "count" instead of props

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -5,12 +5,12 @@ interface CounterProps {
   count: Signal<number>;
 }
 
-export default function Counter(props: CounterProps) {
+export default function Counter({count}: CounterProps) {
   return (
     <div className="flex gap-2 w-full">
-      <p className="flex-grow-1 font-bold text-xl">{props.count}</p>
-      <Button onClick={() => props.count.value -= 1}>-1</Button>
-      <Button onClick={() => props.count.value += 1}>+1</Button>
+      <p className="flex-grow-1 font-bold text-xl">{count}</p>
+      <Button onClick={() => count.value -= 1}>-1</Button>
+      <Button onClick={() => count.value += 1}>+1</Button>
     </div>
   )
 }
