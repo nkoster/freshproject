6a294b35abe67f42fd4a5f7958ee60fbdf06fc08
Niels <n.koster@portavita.eu>
2023-06-22 23:17:15 +0200

    Show params object

--- routes/greet/[name].tsx
+++ routes/greet/[name].tsx
@@ -1,5 +1,12 @@
 import {PageProps} from '$fresh/server.ts'
 
 export default function GreetPage({params}: PageProps) {
-  return <div>Hello {params.name}</div>
+  return (
+    <div>
+      <div>Hello {params.name}</div>
+      <pre>
+        {JSON.stringify(params, null, 2)}
+      </pre>
+    </div>
+  )
 }
