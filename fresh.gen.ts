caf85985aa29af73b2534d8de5eb8c80fc8a3662
Niels <n.koster@portavita.eu>
2023-06-26 00:22:13 +0200

    Add more example components and routes.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,32 +3,44 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/about.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/countdown.tsx";
-import * as $4 from "./routes/github/[username].tsx";
-import * as $5 from "./routes/greet/[name].tsx";
-import * as $6 from "./routes/index.tsx";
-import * as $7 from "./routes/random-uuid.tsx";
-import * as $8 from "./routes/search.tsx";
+import * as $1 from "./routes/_middleware.tsx";
+import * as $2 from "./routes/about.tsx";
+import * as $3 from "./routes/api/joke.ts";
+import * as $4 from "./routes/countdown.tsx";
+import * as $5 from "./routes/github/[username].tsx";
+import * as $6 from "./routes/greet/[name].tsx";
+import * as $7 from "./routes/html.tsx";
+import * as $8 from "./routes/index.tsx";
+import * as $9 from "./routes/my-handler.tsx";
+import * as $10 from "./routes/plain.tsx";
+import * as $11 from "./routes/random-uuid.tsx";
+import * as $12 from "./routes/search.tsx";
+import * as $13 from "./routes/subscribe.tsx";
 import * as $$0 from "./islands/Countdown.tsx";
 import * as $$1 from "./islands/Counter.tsx";
+import * as $$2 from "./islands/GlobalCounter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/about.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/countdown.tsx": $3,
-    "./routes/github/[username].tsx": $4,
-    "./routes/greet/[name].tsx": $5,
-    "./routes/index.tsx": $6,
-    "./routes/random-uuid.tsx": $7,
-    "./routes/search.tsx": $8,
+    "./routes/_middleware.tsx": $1,
+    "./routes/about.tsx": $2,
+    "./routes/api/joke.ts": $3,
+    "./routes/countdown.tsx": $4,
+    "./routes/github/[username].tsx": $5,
+    "./routes/greet/[name].tsx": $6,
+    "./routes/html.tsx": $7,
+    "./routes/index.tsx": $8,
+    "./routes/my-handler.tsx": $9,
+    "./routes/plain.tsx": $10,
+    "./routes/random-uuid.tsx": $11,
+    "./routes/search.tsx": $12,
+    "./routes/subscribe.tsx": $13,
   },
   islands: {
     "./islands/Countdown.tsx": $$0,
     "./islands/Counter.tsx": $$1,
+    "./islands/GlobalCounter.tsx": $$2,
   },
   baseUrl: import.meta.url,
 };
